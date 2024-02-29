805d304f567a1b0787be3adc8cbcc66156cc7f3e
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-02-29 12:21:19 -0300

    mexendo com array e obj

---
+++ cadastro-de-imoveis/imovel.js
@@ -0,0 +1,50 @@
+const imoveis = []
+let opcao = ""
+
+do {
+    opcao = prompt("Bem-vindo ao Cadastro de Imóveis.\nTotal de imóveis: " + imoveis.length + "\n\nEscolha uma opção:\n1. Novo imóvel\n2. Listar novo imóvel\n3. Sair")
+
+    switch (opcao) {
+        case "1":
+            const imovel = {}
+            imovel.proprietario = prompt("Informe o nome do proprietário do imóvel:")
+            imovel.quartos = parseFloat(prompt("Quantos quartos possui o imóvel?"))
+            imovel.banheiros = parseFloat(prompt("Quantos banheiros possui o imóvel?"))
+            imovel.garagem = prompt("O imóvel possui garagem? (Sim/Não)")
+            
+            const confirmar = confirm (
+                "Salvar este imóvel?\n" +
+                "\nProprietário: " + imovel.proprietario +
+                "\nQuartos: " + imovel.quartos +
+                "\nBanheiros: " + imovel.banheiros +
+                "\nPossui Garagem? " + imovel.garagem
+            )
+
+            if (confirmar) {
+                imoveis.push(imovel)
+            } else {
+                alert ("Voltando ao menu.")
+            }
+            break
+        
+        case "2":
+            for (let i = 0; i < imoveis.length; i++) {
+                alert(
+                    "Imóvel " + (i + 1) +
+                    "\nProprietário: " + imoveis[i].proprietario +
+                    "\nQuartos: " + imoveis[i].quartos +
+                    "\nBanheiros: " + imoveis[i].banheiros +
+                    "\nPossui Garagem? " + imoveis[i].garagem
+                )
+            }
+            break
+
+        case "3":
+            alert("Encerrando...")
+            break3
+
+
+        default:
+            alert("Opção inválida")
+    }
+} while (opcao !== "3");
---
+++ matriz/matriz.js
@@ -0,0 +1,31 @@
+// Arrays podem receber qualquer tipo da dado, inclusive outros arrays
+const arr = [
+    "1º Nível",
+    ["2º Nível", 42, true],
+    [
+        ["3º Nível, 1º item", "Olá Mundo!"],
+        ["3º Nível, 2º item", "Oi Mundo!"],
+    ],
+]
+
+console.log(arr[2][1][0])
+
+// Podemos ver claramente a estrutura de uma matriz em uma tabela
+const matriz = [
+    ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+    ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+    ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+]
+
+console.table(matriz)
+matriz[0].push("Nova coluna")
+console.table(matriz)
+
+// Podemos usar vários níveis de laços FOR para iterar facilmente pela matriz
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        const elemento = matriz [i][j]
+        console.log("Posição: (" + i + ", " + j + ") Valor : " + elemento)
+    }
+}
+
