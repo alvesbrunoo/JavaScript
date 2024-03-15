b6f277361773ba9c1e762bddf985a8a5a4c3e500
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-03-15 10:54:08 -0300

    aprendendo dom

--- eventos-no-html/index.js
+++ eventos-no-html/index.js
@@ -1,7 +1,19 @@
-function MouseEvent() {
+function mouseEvent() {
     console.log('mouse entered')
 }
 
 function changeEvent() {
-    console.log('change ')
+    console.log('value change')
+}
+
+function focusEvent() {
+    console.log('gained focus')
+}
+
+function blurEvent() {
+    console.log('lost focus')
+}
+
+function clickEvent() {
+    alert('clicked')
 }
