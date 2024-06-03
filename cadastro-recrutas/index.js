3e76e16e7c4405efb6d7c16c83f185bc72c1490a
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-06-03 09:42:40 -0300

    .

---
+++ cadastro-recrutas/index.js
@@ -0,0 +1,10 @@
+const nome = prompt('Primeiro nome:')
+const sobrenome = prompt('Sobrenome:')
+const campo = prompt('Campo:')
+const nascimento = prompt('Nascimento:')
+
+alert('Nome: ' + nome +
+    '\nSobrenome: ' + sobrenome +
+    '\nCampo de estudo: ' + campo +
+    '\nAno de nascimeto: ' + nascimento
+)
--- calculadora/index.js
+++ calculadora/index.js
@@ -1,18 +1,17 @@
-const entrada1 = prompt("Digite o primeiro número: ")
-const entrada2 = prompt("Digite o segundo número: ")
+const num1 = prompt('Digite o primeiro número:')
+const num2 = prompt('Digite o segundo número:')
 
-const x = parseFloat(entrada1)
-const y = parseFloat(entrada2)
+const x = parseFloat(num1)
+const y = parseFloat(num2)
 
-const soma = x + y 
-const subtracao = x - y 
-const multiplicacao = x * y
+const soma = x + y
+const subtracao = x - y
 const divisao = x / y
+const multiplicacao = x * y
 
 alert(
-    "Resultados:\n" +
     "\nSoma: " + soma +
     "\nSubtração: " + subtracao +
-    "\nDivisão: " + divisao +
-    "\nMultiplicação: " + multiplicacao
-)
+    "\nMultiplicação: " + multiplicacao +
+    "\nDivisão: " + divisao
+  )
