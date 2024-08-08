d28aa3a374dc142344e96feb7ed8bb5cdcda744b
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-08-08 16:45:19 -0300

    .

--- dano/index.js
+++ dano/index.js
@@ -1,24 +1,27 @@
 const atacante = prompt("Qual é o nome do personagem atacante?")
-const poderDeAtaque = parseFloat(prompt("Qual é o seu poder de ataque?"))
+const poderAtaque = parseFloat(prompt("Qual é o seu poder de ataque?"))
 
 const defensor = prompt("Qual é nome do personagem defensor?")
+const poderDefesa = parseFloat(prompt('Qual o poder de defesa?'))
 let pontosDeVida = parseFloat(prompt("Quantos pontos de vida ele possui?"))
-const poderDeDefesa = parseFloat(prompt("Qual é o seu poder de defesa?"))
-const possuiEscudo = prompt("Ele possui um escudo? (Sim/Não)")
+const escudo = prompt('Possui escudo? (Sim/não)')
 
 let danoCausado = 0
 
-if (poderDeAtaque > poderDeDefesa && possuiEscudo === "Não") {
-    danoCausado = poderDeAtaque - poderDeDefesa
-  } else if (poderDeAtaque > poderDeDefesa && possuiEscudo === "Sim") {
-    danoCausado = (poderDeAtaque - poderDeDefesa) / 2
-  }
-  
-  pontosDeVida -= danoCausado
+if (poderAtaque > poderDefesa && escudo === 'Não') {
+  danoCausado = poderAtaque - poderDefesa
+} else if (poderAtaque > poderDefesa && escudo === 'Sim') {
+  danoCausado = (poderAtaque - poderDefesa) / 2
+} else if (poderAtaque < poderDefesa) {
+  danoCausado = 0
+}
 
-  alert(atacante + " causou " + danoCausado + " pontos de dano em " + defensor)
+pontosDeVida -= danoCausado
+
+alert(atacante + ' causou ' + danoCausado + ' pontos de dano ' + defensor) 
+
 alert(
-  atacante + "\nPoder de ataque: " + poderDeAtaque + "\n\n" +
-  defensor + "\nPontos de vida: " + pontosDeVida +
-  "\nPoder de defesa: " + poderDeDefesa + "\nPossui escudo: " + possuiEscudo
+  '\nPoder de ataque: ' + poderAtaque + 
+  defensor + '\nPontos de vida: ' + pontosDeVida + 
+  '\nPontos de defesa: ' + poderDefesa + '\nPossui escudo: ' + escudo
 )
--- velocidade/index.js
+++ velocidade/index.js
@@ -1,13 +1,13 @@
-const vei1 = prompt('Digite o nome do seu veículo')
-const vel1 = parseFloat(prompt('Digite a velocidade do primeiro veículo')) 
+const vei1 = prompt('Qual o nome do primeiro veículo?')
+const vel1 = parseFloat(prompt('Qual a velocidade do primeiro veículo?'))
 
-const vei2 = prompt('Digite o nome do segundo veículo')
-const vel2 = parseFloat(prompt('Digite a velocidade do segundo veículo'))
+const vei2 = prompt('Qual o nome do segundo veículo?')
+const vel2 = parseFloat(prompt('Qual a velocidade do segundo veículo?'))
 
 if (vel1 > vel2) {
-   alert(vei1 + ' é mais rápido do que o ' + vei2)
+   alert(vei1 + ' é ' + ' mais rápido que ' + vei2)
 } else if (vel1 < vel2) {
-   alert(vei2 + ' é mais rápido do que o ' + vei1)
+   alert(vei2 + ' é ' + ' mais rápido que ' + vei1)
 } else {
-   alert(vei1 + ' e ' + vei2 + ' possuem a mesma velocidade ')
+   alert(vei1 + ' e ' + vei2 + ' possuem a mesma velocidade')
 }
