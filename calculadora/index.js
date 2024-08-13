4ac2bc156841009f563df477eb644471fa2f70f4
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-08-13 13:56:33 -0300

    Update index.js

--- calculadora/index.js
+++ calculadora/index.js
@@ -9,37 +9,38 @@ const subtracao = x - y
 const divisao = x / y
 const multiplicacao = x * y
 
+let operacao = ''
 
-const operaçao = prompt('Qual operação você deseja usar?\n' + 
-  '\n1.Soma' +
-  '\n2.Subtrção' +
-  '\n3.Divisão' +
-  '\n4.Multiplicação' +
-  '\n5.Sair' 
+do {
+  operacao = prompt('Qual operação você deseja usar?\n' + 
+    '\n1.Soma' +
+    '\n2.Subtração' +
+    '\n3.Divisão' +
+    '\n4.Multiplicação' +
+    '\n5.Sair' 
+  )
 
-)
-
-switch (operaçao) {
-  case '1':
-    alert(soma)
-    break
-  case '2':
-    alert(subtracao)
-    break
-  case '3':
-    alert(divisao)
-    break
-  case '4':
-    alert(multiplicacao)
-    break
-  case '5':
-    alert('Saindo...')
-    break
-  default:
-    alert('Opção inválida!')
-    break
-}
-
+  switch (operacao) {
+    case '1':
+      alert(soma)
+      break
+    case '2':
+      alert(subtracao)
+      break
+    case '3':
+      alert(divisao)
+      break
+    case '4':
+      alert(multiplicacao)
+      break
+    case '5':
+      alert('Encerrando...')
+      break
+    default:
+      alert('Opção inválida!')
+      break
+  }
+} while (operacao !== '5')
 
 
 // alert (
