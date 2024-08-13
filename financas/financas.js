abc483d5a45823befd58b592389413600ce1f381
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-08-13 11:51:11 -0300

    while

--- financas/financas.js
+++ financas/financas.js
@@ -1,24 +1,27 @@
-let saldo = parseFloat(prompt("Informe a quantidade de dinheiro inicial:"))
+let saldo = parseFloat(prompt('Qual é o seu saldo atual?'))
+
 let opcao = ""
 
 do {
-    opcao = prompt("Saldo disponível: R$ " + saldo +
-    "\n1. Adicionar dinheiro" +
-    "\n2. Remover dinheiro" +
-    "\n3. Sair")
+    opcao = prompt('Escolha o que você deseja fazer:' +
+        '\nSaldo disponível: R$ ' + saldo +
+        '\n1.Adicionar dinheiro' +
+        '\n2.Remover dinheiro' +
+        '\n3.Encerrar' 
+    )
 
     switch (opcao) {
-        case "1":
-            saldo += parseFloat(prompt("Infome o valor a ser adicionado: "))
+        case '1':
+            saldo += parseFloat(prompt('Qual a quantidade de dinheiro que deseja colocar?'))
             break
-        case "2":
-            saldo -= parseFloat(prompt("Infome o valor a ser removido: "))
+        case '2':
+            saldo -= parseFloat(prompt('Qual a quantidade de dinheiro que deseja remover?'))
             break
-        case "3":
-            alert("Saindo...")
+        case '3':
+            alert('Encerrando...')
             break
         default:
-            alert("Opção inválida")
-            break
+            alert('Opção inválida')
     }
-}while (opcao !== "3");
+    
+} while (opcao !== '3')
--- menu-interativo/menu-interativo.js
+++ menu-interativo/menu-interativo.js
@@ -1,29 +1,34 @@
 let opcoes = ""
-do { opcoes = prompt("Seja bem-vindo(a)!\n" +
-"\n1. - Opção Um" +
-"\n2. - Opção Dois" +
-"\n3. - Opção Três" +
-"\n4. - Opção Qautro" +
-"\n5. - Encerrar") 
 
-switch (opcoes) {
-    case "1":
-        alert("Você escolheu a opção 1")
-        break
-    case "2":
-        alert("Você escolheu a opção 2")
-        break
-    case "3":
-        alert("Você escolheu a opção 3")
-        break
-    case "4":
-        alert("Você escolheu a opção 4")
-        break
-    case "5":
-        alert("Encerrando o programa...")
-        break
-    default:
-        alert("Opção inválida")
-}
+do {
+    opcoes = prompt(
+        'Escolha uma opção:' +
+        '\n1.Opção um' +
+        '\n2.Opção dois' +
+        '\n3.Opção três' +
+        '\n4.Opção quatro' +
+        '\n5.Encerrar'
+    )
 
-} while (opcoes !== "5");
+    switch (opcoes) {
+        case '1':
+            alert('Você escolheu a opção 1')
+            break
+        case '2':
+            alert('Você escolheu a opção 2')
+            break
+        case '3':
+            alert('Você escolheu a opção 3')
+            break
+        case '4':
+            alert('Você escolheu a opção 4')
+            break
+        case '5':
+            alert('Encerrando...')
+            break
+        default:
+            alert('Opção inválida')
+            break
+    }
+
+} while(opcoes !=='5')
