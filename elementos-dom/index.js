3e9839283a346f196a3fa774b37e3edf921dff78
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-08-19 12:59:08 -0300

    dom

--- elementos-dom/index.js
+++ elementos-dom/index.js
@@ -2,14 +2,13 @@ function addInput() {
     const ul = document.getElementById('inputs')
 
     const newLi = document.createElement('li')
-    newLi.ClassName = 'list-item'
+    newLi.className = 'list-item'
     newLi.innerText = 'Novo input: '
 
     const newInput = document.createElement('input')
     newInput.type = 'text'
     newInput.name = 'input'
 
-    
     newLi.appendChild(newInput)
     ul.appendChild(newLi)
 }
