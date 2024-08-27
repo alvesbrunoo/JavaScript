4d6aefb62b0847adf0d2480ed3eeabdf4e21f77f
alvesdesouzabruno <151651480+alvesbrunoo@users.noreply.github.com>
2024-08-27 12:26:14 -0300

    Update matriz.js

--- matriz/matriz.js
+++ matriz/matriz.js
@@ -1,16 +1,18 @@
-// Arrays podem receber qualquer tipo da dado, inclusive outros arrays
 const arr = [
-    "1º Nível",
-    ["2º Nível", 42, true],
+    '1º Nível',
+    ['2º Nível', 42, true],
     [
-        ["3º Nível, 1º item", "Olá Mundo!"],
-        ["3º Nível, 2º item", "Oi Mundo!"],
+        ['3º nível, 1º item', 'Olá, mundo!'],
+        ['3º nível, 2º item', 'Olá, mundo!'],
     ],
+    []
 ]
 
-console.log(arr[2][1][0])
+console.log(arr[0])
+console.log(arr[1][1])
+console.log(arr[2][0])
+console.log(arr[2][1][1])
 
-// Podemos ver claramente a estrutura de uma matriz em uma tabela
 const matriz = [
     ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
     ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
@@ -18,14 +20,16 @@ const matriz = [
 ]
 
 console.table(matriz)
-matriz[0].push("Nova coluna")
+
+matriz.push(['Nova linha'])
+matriz[0].push('Nova coluna')
+
+
 console.table(matriz)
 
-// Podemos usar vários níveis de laços FOR para iterar facilmente pela matriz
 for (let i = 0; i < matriz.length; i++) {
-    for (let j = 0; j < matriz[i].length; j++) {
-        const elemento = matriz [i][j]
-        console.log("Posição: (" + i + ", " + j + ") Valor : " + elemento)
+    for(let j = 0; j < matriz.length; j++) {
+        const elemento = matriz[i][j]
+        console.log('Posição: (' + i + ', ' + j +') Valor: ' + elemento)
     }
 }
-
